5fb71216f35ef77caaaac227e7b5823271e8603f
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-31 12:25:03 -0700

    implement backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
 import Header from './components/Header/Header'
 import SearchPage from './components/SearchPage/SearchPage';
-import SearchResult from './components/SearchResult/SearchResult';
 
 function App() {
+
+  const [isSearchBarClicked, setIsSearchBarClicked] = useState(false);
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header setIsSearchBarClicked={setIsSearchBarClicked} />
       <Routes>
-        <Route path='/search-page' element={<SearchPage />} />
-        <Route path='/search-result' element={<SearchResult />} />
+        <Route path='/search-page' element={<SearchPage isSearchBarClicked={isSearchBarClicked} setIsSearchBarClicked={setIsSearchBarClicked} />} />
+        {/* <Route path='/search-result' element={<SearchResult />} /> */}
       </Routes>
     </BrowserRouter>
   )
