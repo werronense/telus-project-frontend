32d3026ed06328c8a6525acec17d903e786aec3b
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-30 23:42:55 -0700

    change header component logo link path to /search-page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header () {
     return(
         <header className='header'>
-            <Link className='header__logo-link' to='/SearchPage'>
+            <Link className='header__logo-link' to='/search-page'>
                 <img className='header__logo-img' src={telusLogo} alt="Telus Logo" />
             </Link>
         </header>
