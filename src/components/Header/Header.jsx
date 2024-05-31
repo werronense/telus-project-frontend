a3fe8bbb5943cfad5d52e5e1f5b9a48305f3e4f3
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-30 22:02:13 -0700

    create Header component and setup code for export; add Header import to App.jsx; re-add index.scss; comment out route for Search component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.scss';
 
 function Header () {
-    <header>
-        <div>abc</div>
-    </header>
+    return(
+        <header>
+            <div>abc</div>
+        </header>
+    );
 }
 
 export default Header;
